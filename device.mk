0fcd608e70e25f960b6d7593a00198b4e1febecd
mint <mint2792@naver.com>
2026-06-09 18:04:39 +0900

    elitelte: avoid broad qcom shell startup

--- device.mk
+++ device.mk
@@ -63,6 +63,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor \
     android.hardware.wifi@1.0-service \
+    libtinycompress \
     libxml2 \
     copybit.msm8937 \
     gralloc.msm8937 \
