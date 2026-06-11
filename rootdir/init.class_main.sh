8fd754864a8cc3da8b84598254bdf5b3a1124bc5
mint <mint2792@naver.com>
2026-06-12 05:12:02 +0900

    elitelte: wire up post-bringup hardware services

--- rootdir/init.class_main.sh
+++ rootdir/init.class_main.sh
@@ -41,9 +41,8 @@ esac
 
 case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3")
+    start irsc_util
     start qmuxd
-    start ipacm-diag
-    start ipacm
     case "$baseband" in
         "svlte2a" | "csfb")
           start qmiproxy
